3917fa204d044380a35b123fb4494c8f61fa0c90
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-03-03 21:33:35 +0700

    update: deploy

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -17,10 +17,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
+  cors()
+  //   {
+  //   origin: process.env.FRONTEND_URL,
+  //   credentials: true,
+  // }
 );
 
 app.use(express.static(path.join(__dirname, "../../front-end/dist")));
